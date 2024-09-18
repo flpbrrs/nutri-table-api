7fbac1bea6c612e6ae760b6cd0aa2e173a38b0ae
Felipe Barros <felipejonathan.dev@gmail.com>
2024-09-17 21:38:53 -0300

    feat: configurando o servidor para usar os objetos criados

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import express from 'express'
+import cors from 'cors'
+
+import alimentosRouter from './routes/alimentos'
 
 const app = express()
+
+app.use(cors())
+
+app.use('/alimentos', alimentosRouter)
 
 app.listen(8080, () => {
     console.log('Server port running on 8080 :rocket:')
